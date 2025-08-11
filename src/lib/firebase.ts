d1af038372dbe4d85191934918ae4032a8da7d64
Jorge Costa <jorgebsjcosta@gmail.com>
2025-08-11 02:34:46 +0000

    FirebaseError: Firebase: Error (auth/api-key-not-valid.-please-pass-a-va

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,5 +1,5 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp, getApps } from "firebase/app";
+import { initializeApp, getApps, getApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
@@ -13,13 +13,8 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-let app;
-if (!getApps().length) {
-  app = initializeApp(firebaseConfig);
-} else {
-  app = getApps()[0];
-}
-
+// To prevent re-initialization on hot-reloads, we check if an app is already initialized.
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 const auth = getAuth(app);
 const db = getFirestore(app);
 
