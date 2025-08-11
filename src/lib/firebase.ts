51438ef0618efc28b0a968df1810b42fafacdc96
Jorge Costa <jorgebsjcosta@gmail.com>
2025-08-11 02:38:45 +0000

    FirebaseError: Firebase: Error (auth/configuration-not-found).

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,13 +1,18 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp, getApps, getApp } from "firebase/app";
+import { initializeApp, getApps, getApp, FirebaseApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { firebaseConfig } from "@/lib/firebase-config";
 
-
 // Initialize Firebase
-// To prevent re-initialization on hot-reloads, we check if an app is already initialized.
-const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+let app: FirebaseApp;
+
+if (getApps().length === 0) {
+  app = initializeApp(firebaseConfig);
+} else {
+  app = getApp();
+}
+
 const auth = getAuth(app);
 const db = getFirestore(app);
 
